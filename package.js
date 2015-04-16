fad94ca63d331b406b826e8de4ef322171544d84
Kuba Wyrobek <kuba.wyrobek@gmail.com>
2015-04-16 12:02:59 +0200

    removed dependency; bumped version

--- package.js
+++ package.js
@@ -1,27 +1,26 @@
-Package.describe({
-  name: 'parhelium:ui-input-selectize',
-  version: '0.0.3',
-  summary: 'Integration of Selectize with Meteor.',
-  git: 'https://github.com/parhelium/meteor-ui-input-selectize',
-  documentation: 'README.md'
-});
+Package.describe( {
+    name: 'parhelium:ui-input-selectize',
+    version: '0.0.4',
+    summary: 'Integration of Selectize with Meteor.',
+    git: 'https://github.com/parhelium/meteor-ui-input-selectize',
+    documentation: 'README.md'
+} );
 
 client = ['client'];
 server = ['server'];
-both   = ['client', 'server'];
+both = ['client', 'server'];
 
-Package.onUse(function(api) {
-  api.versionsFrom('1.0.3.1');
+Package.onUse( function ( api ) {
+    api.versionsFrom( '1.0.3.1' );
 
-  api.use([
-      'templating',
-      'jeremy:selectize',
-      'parhelium:logger',
-      'reactive-var',
-  ], client);
+    api.use( [
+        'templating',
+        'jeremy:selectize@0.12.1',
+        'reactive-var',
+    ], client );
 
-  api.addFiles( [
-      'lib/selectize.html',
-      'lib/selectize.js'
-  ], client);
-});
+    api.addFiles( [
+        'lib/selectize.html',
+        'lib/selectize.js'
+    ], client );
+} );
